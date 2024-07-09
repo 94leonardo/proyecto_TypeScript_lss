bc4910aa802a4b7530e2519313a910715c80b1bc
leonardolasso <lassoleonardo56@gmail.com>
2024-07-08 22:26:32 -0500

    primer commit main script

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+// Definir variables en typeScript
+
+
+let nombre:string = "leonardo lasso";
+console.log("Variable texto: ", nombre);
+
+let numero:number = 2024;
+console.log("Variable numérica: ", numero);
+
+let verdadero = true;
+verdadero = false;
+console.log("Variable booleana: ", verdadero);
+
+let cualquier:any = "puedo asignarle lo que quiera";
+console.log("Variable any: ", cualquier);
+
+// Constantes
+const nombres:string = "leonardo";
+
+// Arrays
+let personas:string[] = ["leonardo", "lasso", "Diana", "Carlos"];
+console.log(personas);
+
+let div_personas:HTMLElement | null = document.querySelector("#person");
+
+let htmlContent = "<ul>";
+console.log(htmlContent);
+
+personas.map((persona) => {
+  htmlContent += `<li>${persona}</li>`; // Alt+96
+});
+
+htmlContent += "</ul>";
+
+console.log(htmlContent);
+
+console.log("div_personas", div_personas);
+
+if (div_personas) {
+  div_personas.innerHTML = htmlContent;
+}
